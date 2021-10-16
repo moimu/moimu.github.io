17492a5bd5a466760eaee6d7cb223a3a5f8fe975
moises <correodemomuar@gmail.com>
2021-10-16 17:03:28 +0200

    layout and styles

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const smserror = document.querySelectorAll('.labelsmserror');
+
+
+
+
+button.addEventListener('click', function verifica(){
+   
+});
+
+
+
+
+input.addEventListener('invalid', function muestrasmserror() {
+    smserror.style.opacity = 0;
+    
+} );
